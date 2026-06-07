8a3d7f4252c5e883b79e85aaf10f73f11f5e91df
Ruawd <Ruawd@noreply.gitcode.com>
2026-06-07 22:36:37 +0800

    Fix curriculum filter visibility

--- src/templates.mjs
+++ src/templates.mjs
@@ -1,7 +1,7 @@
 import { site } from "./site-data.mjs";
 
 const baseUrl = `https://${site.domain}`;
-const assetVersion = "20260607-cleanurls";
+const assetVersion = "20260607-filterfix";
 
 export function renderPage(page, pages) {
   const canonicalPath = page.href || "/";
